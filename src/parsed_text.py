bab16c1e34d7c9d2892cda6e9cbe45d0d8dfa9a3
Thomas Applencourt <applencourt@anl.gov>
2017-01-31 14:35:55 -0600

    Fix toucher 2.

--- src/parsed_text.py
+++ src/parsed_text.py
@@ -507,15 +507,15 @@ def build_needs(parsed_text, subroutines, stuple, variables):
     #    - Create the pointer copy
     #   -  Add the value (so it add also to the pointer reference...)
     
-#    for v in variables:
-#        main = variables[v].same_as
-#        if main != v:
-#            variables[v].needed_by = variables[main].needed_by
+    for v in variables:
+        main = variables[v].same_as
+        if main != v:
+            variables[v].needed_by = variables[main].needed_by
 
     for v in variables:
         var = variables[v]
-#        if var.is_main:
-        for x in var.needs:
+        if var.is_main:
+            for x in var.needs:
                 variables[x].needed_by.append(var.name)
 
     for var in variables.values():
